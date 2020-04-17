4ca250475abbd5ecfeabf5a68fbd2e5ec7de1bef
tu <liulangdeerhu@qq.com>
2020-04-17 15:15:38 +0800

    7.8.3节完毕

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
 
     #path('/lists/the-only-list-in-the-world/',views.view_list,name='view_list'),
     url('^lists/the-only-list-in-the-world/$',views.view_list,name='view_list'),
+    url(r'^lists/new$',views.new_list,name='new_list'),
 ]
