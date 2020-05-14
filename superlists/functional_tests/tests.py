89f068c90ac5b8e42bfe0c579f308314a43fcff4
tu <liulangdeerhu@qq.com>
2020-05-14 22:06:17 +0800

    在前面的章节中功能测试有遗漏的地方，在这里补全

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,43 +1,49 @@
-import os
-import time
-
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+import sys
+import time
 
-MAX_WAIT=10
+MAX_WAIT = 10
+
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
     def setUp(self):
-        self.browser=webdriver.Firefox()
-        staging_server=os.environ.get('39.99.160.70')
-        if staging_server:
-            self.live_server_url='http://'+staging_server
+        self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
+
     def wait_for_row_in_list_table(self, row_text):
-        start_time=time.time()
+        start_time = time.time()
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
-                rows=table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text,[row.text for row in rows])
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
                 return
-            except (AssertionError,WebDriverException) as e:
-                if time.time() - start_time >MAX_WAIT:
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
 
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -71,18 +77,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
         # Satisfied, she goes back to sleep
 
-    def test_multiple_user_can_start_lists_at_different_urls(self):
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith start a new todo list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
@@ -101,7 +101,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -125,9 +125,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
@@ -135,7 +136,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=5
         )
 
         # She starts a new list and sees the input is nicely
@@ -147,5 +148,5 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=5
         )
