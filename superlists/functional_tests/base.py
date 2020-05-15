4138515f462ac990995f2f10f3e96cc0340aec57
tu <liulangdeerhu@qq.com>
2020-05-15 21:42:12 +0800

    Move unite tests into a folder with single file

--- superlists/functional_tests/base.py
+++ superlists/functional_tests/base.py
@@ -1,3 +1,4 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -6,12 +7,30 @@ import time
 MAX_WAIT = 10
 
 class FunctionalTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
+    def wait_for(self,fn):
+        start_time=time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError,WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
